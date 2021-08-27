6ff5ba5c819cebda4e95583bf26fb484a12f4f94
Eder Sena <eder.sena@live.com>
2021-08-26 21:55:51 -0300

    dockermongo register

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,13 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +16,23 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner runner(StudentRepository repository) {
+		return args -> {
+			Address address = new Address("England", "London", "0000000");
+
+			Student student = new Student(
+					"Jamila",
+					"Ahmed",
+					"jahmed@gmail.com",
+					Gender.FEMALE,
+					address,
+					List.of("Computer Science", "Maths"),
+					BigDecimal.TEN,
+					LocalDateTime.now()
+			);
+
+			repository.insert(student);
+		};
+	}
 }
